9adb6835ed9e6a04b2a162938ea0fbe150834965
michh18 <michellechan0518@gmail.com>
2026-04-13 10:28:46 +0100

    feat: implement record classes for the Position of rover and PlateauSize

---
+++ MarsRover.Console/PlateauSize.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Console
+{
+    public record PlateauSize(int maxX, int maxY);
+}
---
+++ MarsRover.Console/Position.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public record Position (int x, int y, CompassDirection facing);
+}
