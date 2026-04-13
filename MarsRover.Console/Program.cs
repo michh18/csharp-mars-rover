b721e2f0235351a17fc73b659d1980179c862457
michh18 <michellechan0518@gmail.com>
2026-04-13 21:41:03 +0100

    feat: Implement console interface for parsing plateau, position and instructions

--- MarsRover.Console/Program.cs
+++ MarsRover.Console/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter plateau size: ");
+            string plateauInput = Console.ReadLine();
+            (int,int) processedPlateau = InputParser.PlateauParser(plateauInput);
+            Console.WriteLine($"Reformatted Plateau: {processedPlateau}");
+
+
+            Console.WriteLine("Enter starting position: ");
+            string positionInput = Console.ReadLine();
+            Position processedPosition = InputParser.PositionParser(positionInput);
+            Console.WriteLine($"Reformatted Position: {processedPosition.ToString()}");
+
+            Console.WriteLine("Enter instructions: ");
+            string instructions1 = Console.ReadLine();
+            List<Instruction> processedInstructions = InputParser.InstructionParser(instructions1);
+            string reformattedInstructions = String.Join("", processedInstructions);
+            Console.WriteLine($"Reformatted Instructions: {reformattedInstructions}");
+
         }
     }
 }
