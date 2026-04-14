f8b6b40092350c4fb50e11ee438d62b89b85a82c
michh18 <michellechan0518@gmail.com>
2026-04-14 14:34:00 +0100

    feat: Apply input instructions to move/rotate the Rover

--- MarsRover.Console/Program.cs
+++ MarsRover.Console/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MarsRover
+﻿using System.Runtime.CompilerServices;
+
+namespace MarsRover
 {
     public class Program
     {
@@ -6,7 +8,7 @@
         {
             Console.WriteLine("Enter plateau size: ");
             string plateauInput = Console.ReadLine();
-            (int,int) processedPlateau = InputParser.PlateauParser(plateauInput);
+            PlateauSize processedPlateau = InputParser.PlateauParser(plateauInput);
             Console.WriteLine($"Reformatted Plateau: {processedPlateau}");
 
 
@@ -16,10 +18,31 @@
             Console.WriteLine($"Reformatted Position: {processedPosition.ToString()}");
 
             Console.WriteLine("Enter instructions: ");
-            string instructions1 = Console.ReadLine();
-            List<Instruction> processedInstructions = InputParser.InstructionParser(instructions1);
+            string instructions = Console.ReadLine();
+            List<Instruction> processedInstructions = InputParser.InstructionParser(instructions);
             string reformattedInstructions = String.Join("", processedInstructions);
             Console.WriteLine($"Reformatted Instructions: {reformattedInstructions}");
+
+            Position currentPosition = processedPosition;
+            
+            Console.WriteLine($"\nStarting Position: {currentPosition.ToString()}" );
+
+            foreach (Instruction instruction in processedInstructions)
+            {
+                if (instruction == Instruction.L || instruction == Instruction.R)
+                {
+                    var newFacing = Rover.Rotate(currentPosition.facing, instruction);
+                    currentPosition = new Position(currentPosition.x, currentPosition.y, newFacing);
+                }
+
+                else if (instruction == Instruction.M) 
+                {
+                    var positionAfterInstruction = Rover.Move(processedPlateau, currentPosition, instruction);
+                    currentPosition = new Position(positionAfterInstruction.x, positionAfterInstruction.y, positionAfterInstruction.facing);
+                }
+            }
+
+            Console.WriteLine($"New position after instructions is: {currentPosition.ToString()}");
 
         }
     }
