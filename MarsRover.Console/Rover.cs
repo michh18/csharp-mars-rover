9d04b9b08fadbdb0ce6e0a49c94a5db6fdcc7c34
michh18 <michellechan0518@gmail.com>
2026-04-14 11:45:23 +0100

    feat: implement and test Move method on rover

--- MarsRover.Console/Rover.cs
+++ MarsRover.Console/Rover.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,43 @@ namespace MarsRover
             }
             int newFacingIndex = ((int)facing + (int)rotateInstruction + 4) % 4;
             return (CompassDirection)newFacingIndex;
+        }
+
+        public static Position Move(PlateauSize plateauSize, Position startPosition, Instruction moveInstruction) 
+        { 
+            if (moveInstruction != Instruction.M)
+            {
+                throw new FormatException("Invalid move instruction.");
+            }
+
+            switch (startPosition.facing) 
+            {
+                case CompassDirection.N:
+                    if (startPosition.y + 1>= plateauSize.maxY) 
+                    {
+                        throw new ArgumentOutOfRangeException("Rover moved out of plateau.");
+                    }
+                    return new Position(startPosition.x, startPosition.y + 1, CompassDirection.N);
+                case CompassDirection.E:
+                    if (startPosition.x + 1 >= plateauSize.maxX)
+                    {
+                        throw new ArgumentOutOfRangeException("Rover moved out of plateau.");
+                    }
+                    return new Position(startPosition.x + 1, startPosition.y, CompassDirection.E);
+                case CompassDirection.S:
+                    if (startPosition.y - 1 < 0)
+                    {
+                        throw new ArgumentOutOfRangeException("Rover moved out of plateau.");
+                    }
+                    return new Position(startPosition.x, startPosition.y - 1, CompassDirection.S);
+                case CompassDirection.W:
+                    if (startPosition.x - 1 < 0)
+                    {
+                        throw new ArgumentOutOfRangeException("Rover moved out of plateau.");
+                    }
+                    return new Position(startPosition.x - 1, startPosition.y, CompassDirection.W);
+            }
+            return null;
         }
     }
 }
--- MarsRover.Tests/Rover_Tests.cs
+++ MarsRover.Tests/Rover_Tests.cs
@@ -9,63 +9,85 @@ using System.Threading.Tasks;
 
 namespace MarsRover.Tests
 {
-    internal class Rover_Tests
+    public class Rover_Tests
     {
-        // ================================ Rotate Tests =========================================
-        [TestCase(CompassDirection.N, Instruction.R, CompassDirection.E)]
-        [TestCase(CompassDirection.E, Instruction.R, CompassDirection.S)]
-        [TestCase(CompassDirection.S, Instruction.R, CompassDirection.W)]
-        [TestCase(CompassDirection.W, Instruction.R, CompassDirection.N)]
-        public void Rotate_ShouldReturnCorrectDirection_WhenRotatingRight(CompassDirection facing, Instruction rotateInstruction, CompassDirection expected)
+        class RotateTests
         {
-            var result = Rover.Rotate(facing, rotateInstruction);
-            Assert.That(result, Is.EqualTo(expected));
-        }
+            // ================================ Rotate Tests =========================================
+            [TestCase(CompassDirection.N, Instruction.R, CompassDirection.E)]
+            [TestCase(CompassDirection.E, Instruction.R, CompassDirection.S)]
+            [TestCase(CompassDirection.S, Instruction.R, CompassDirection.W)]
+            [TestCase(CompassDirection.W, Instruction.R, CompassDirection.N)]
+            public void Rotate_ShouldReturnCorrectDirection_WhenRotatingRight(CompassDirection facing, Instruction rotateInstruction, CompassDirection expected)
+            {
+                var result = Rover.Rotate(facing, rotateInstruction);
+                Assert.That(result, Is.EqualTo(expected));
+            }
 
-        [TestCase(CompassDirection.N, Instruction.L, CompassDirection.W)]
-        [TestCase(CompassDirection.E, Instruction.L, CompassDirection.N)]
-        [TestCase(CompassDirection.S, Instruction.L, CompassDirection.E)]
-        [TestCase(CompassDirection.W, Instruction.L, CompassDirection.S)]
-        public void Rotate_ShouldReturnCorrectDirection_WhenRotatingLeft(CompassDirection facing, Instruction rotateInstruction, CompassDirection expected)
-        {
-            var result = Rover.Rotate(facing, rotateInstruction);
-            Assert.That(result, Is.EqualTo(expected));
-        }
+            [TestCase(CompassDirection.N, Instruction.L, CompassDirection.W)]
+            [TestCase(CompassDirection.E, Instruction.L, CompassDirection.N)]
+            [TestCase(CompassDirection.S, Instruction.L, CompassDirection.E)]
+            [TestCase(CompassDirection.W, Instruction.L, CompassDirection.S)]
+            public void Rotate_ShouldReturnCorrectDirection_WhenRotatingLeft(CompassDirection facing, Instruction rotateInstruction, CompassDirection expected)
+            {
+                var result = Rover.Rotate(facing, rotateInstruction);
+                Assert.That(result, Is.EqualTo(expected));
+            }
 
-        [TestCase(CompassDirection.N, Instruction.M)]
-        public void Rotate_ShouldThrowFormatException_WhenUsingInstructionM(CompassDirection facing, Instruction rotateInstruction)
-        {
-            string expected = "Invalid rotation instruction.";
-            var ex = Assert.Throws<FormatException>(() => Rover.Rotate(facing, rotateInstruction));
-            Assert.That(ex.Message.ToString(), Does.Contain(expected));
+            [TestCase(CompassDirection.N, Instruction.M)]
+            public void Rotate_ShouldThrowFormatException_WhenUsingInstructionM(CompassDirection facing, Instruction rotateInstruction)
+            {
+                string expected = "Invalid rotation instruction.";
+                var ex = Assert.Throws<FormatException>(() => Rover.Rotate(facing, rotateInstruction));
+                Assert.That(ex.Message.ToString(), Does.Contain(expected));
+            }
         }
+        // ================================ Move Tests =========================================
+        class MoveTests
+        {
+            [Test]
+            public void Move_ShouldReturnCorrectPosition_WhenMoving()
+            {
+                var plateauSize = new PlateauSize(5, 5);
+                var moveInstruction = Instruction.M;
 
-        [TestCase("1")]
-        [TestCase("1 2 3")]
-        public void PlateauParser_ShouldThrowArgumentException_WhenStringDoesNotContainExactlyTwoIntegers(string input)
-        {
-            string expected = "Need a string of exactly 2 numbers!";
-            var ex = Assert.Throws<ArgumentException>(() => InputParser.PlateauParser(input));
-            Assert.That(ex.Message.ToString(), Does.Contain(expected));
-        }
+                var startPosition1 = new Position(0, 0, CompassDirection.N);
+                var startPosition2 = new Position(0, 0, CompassDirection.E);
+                var startPosition3 = new Position(5, 5, CompassDirection.S);
+                var startPosition4 = new Position(5, 5, CompassDirection.W);
 
-        [TestCase("a 1")]
-        [TestCase("a b")]
-        public void PlateauParser_ShouldThrowFormatException_WhenStringContainsSomeAndNoIntegers(string input)
-        {
-            string expected = "Input must contain valid integers.";
-            var ex = Assert.Throws<FormatException>(() => InputParser.PlateauParser(input));
-            Assert.That(ex.Message.ToString(), Does.Contain(expected));
-        }
+                var result1 = new Position(0, 1, CompassDirection.N);
+                var result2 = new Position(1, 0, CompassDirection.E);
+                var result3 = new Position(5, 4, CompassDirection.S);
+                var result4 = new Position(4, 5, CompassDirection.W);
 
-        [TestCase("-5 -5")]
-        [TestCase("-5 0")]
-        [TestCase("0 0")]
-        public void PlateauParser_ShouldThrowArgumentOutOfRangeException_WhenParsedStringOfNegativeIntsOrBothZeros(string input)
-        {
-            string expected = "Coordinates must be positive integers.";
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => InputParser.PlateauParser(input));
-            Assert.That(ex.Message.ToString(), Does.Contain(expected));
-        }
+                var startPositionList = new List<Position>() { startPosition1, startPosition2, startPosition3, startPosition4 };
+                var resultList = new List<Position>() { result1, result2, result3, result4 };
+                for (int i = 0; i < startPositionList.Count; i++)
+                {
+                    var result = Rover.Move(plateauSize, startPositionList[i], moveInstruction);
+                    Assert.That(result, Is.EqualTo(resultList[i]));
+                }
+            }
+            [Test]
+            public void Move_ShouldThrowArgumentOutOfRangeException_WhenRoverMovesOutOfPlateau()
+            {
+                var plateauSize = new PlateauSize(5, 5);
+                var moveInstruction = Instruction.M;
+
+                var startPosition1 = new Position(5, 5, CompassDirection.N);
+                var startPosition2 = new Position(5, 5, CompassDirection.E);
+                var startPosition3 = new Position(0, 0, CompassDirection.S);
+                var startPosition4 = new Position(0, 0, CompassDirection.W);
+
+                var startPositionList = new List<Position>() { startPosition1, startPosition2, startPosition3, startPosition4 };
+                foreach (var startPosition in startPositionList)
+                {
+                    string expected = "Rover moved out of plateau.";
+                    var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Rover.Move(plateauSize, startPosition, moveInstruction));
+                    Assert.That(ex.Message.ToString(), Does.Contain(expected));
+                }   
+            }
+        } 
     }
 }
